352aebfe2f2229d3ff05a80dd964abca9f790ba8
N3073 <atte.rehnback@gmail.com>
2025-08-14 22:41:39 +0300

    Added collision detection and bullets!

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,9 +16,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids,updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     player = Player(x=SCREEN_WIDTH/2,y=SCREEN_HEIGHT/2)
     asteroid_field = AsteroidField()
     while 1:
@@ -28,6 +31,14 @@ def main():
         screen.fill("black")
         for thing in updatable:
             thing.update(dt)
+        for asteroid in asteroids:
+            if player.does_collide(asteroid):
+                print("Game over!")
+                return
+            for bullet in shots:
+                if asteroid.does_collide(bullet):
+                    asteroid.kill()
+                    bullet.kill()
         for thing in drawable:
             thing.draw(screen)
         #last step
--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
-
+        self.shot_cooldown = 0
     # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -28,7 +29,8 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
-
+        self.shot_cooldown -= dt
+        self.shot_cooldown = max(self.shot_cooldown,0)
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -37,3 +39,10 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
+        if keys[pygame.K_SPACE] and self.shot_cooldown==0:
+            self.shoot()
+
+    def shoot(self):
+        self.shot_cooldown = PLAYER_SHOOT_COOLDOWN
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)*PLAYER_SHOOT_SPEED
