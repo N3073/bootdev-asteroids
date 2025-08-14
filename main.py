fa0cac6ec15227f62795635bc72c09d805c5f85a
N3073 <atte.rehnback@gmail.com>
2025-08-14 20:20:46 +0300

    Added player moving and rendering

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    game_clock=pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x=SCREEN_WIDTH/2,y=SCREEN_HEIGHT/2)
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
-
+        for thing in updatable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         #last step
         pygame.display.flip()
+        dt=game_clock.tick(60)/1000
 
 
 if __name__ == "__main__":
